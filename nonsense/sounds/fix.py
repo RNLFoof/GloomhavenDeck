92bacabb03ee78cb2580a9944dcb68f510150be6
RNLFoof <z.zablotsky@gmail.com>
2022-07-05 19:00:10 -0400

    sound stuff

--- nonsense/sounds/fix.py
+++ nonsense/sounds/fix.py
@@ -3,7 +3,7 @@ import os
 originaldir = "originals"
 fixeddir = r"..\..\app\src\main\res\raw"
 for originalfilename in os.listdir(originaldir):
-    newfilename = originalfilename.replace(" ", "_")
+    newfilename = originalfilename.replace(" ", "_").lower()
     print(originalfilename)
     os.system(f'ffmpeg -i "{os.path.join(originaldir, originalfilename)}"  -af silenceremove=1:0:-50dB -af loudnorm -vn -ar 44100 -ac 2 -b:a 192k LOL.mp3')
     if os.path.exists(os.path.join(fixeddir, newfilename)):
