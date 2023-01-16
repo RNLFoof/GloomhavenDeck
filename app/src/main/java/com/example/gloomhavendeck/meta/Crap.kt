009da96a94056e7cd067f034c7f2c3e4b1cbff63
RNLFoof <z.zablotsky@gmail.com>
2023-01-16 17:55:49 -0500

    Test stuff

--- app/src/main/java/com/example/gloomhavendeck/meta/Crap.kt
+++ app/src/main/java/com/example/gloomhavendeck/meta/Crap.kt
@@ -1,18 +1,35 @@
 package com.example.gloomhavendeck.meta
 
-import com.example.gloomhavendeck.Controller
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.example.gloomhavendeck.*
 import java.util.*
 import kotlin.reflect.KMutableProperty
 import kotlin.reflect.full.memberProperties
 
 open class Crap() {
     companion object {
+
         inline fun <reified T> fieldsFromInto(from: T, to: T) where T : Any {
             for (property in T::class.memberProperties) {
                 if (property is KMutableProperty<*>) {
                     property.setter.call(to, property.get(from))
                 }
             }
+        }
+
+        @RequiresApi(Build.VERSION_CODES.O)
+        fun allControllables(): Array<Controllable> {
+            // I don't know how to not do this by hand. aaaaaaaa
+            return arrayOf(
+                Deck(),
+                Inventory(),
+                Logger(),
+                Player(maxHp = 26),
+                Saver(filesDir = ""),
+                UndoManager(),
+                UndoPoint()
+            )
         }
     }
 }
--- app/src/test/java/com/example/gloomhavendeck/UndoManagerTest.kt
+++ app/src/test/java/com/example/gloomhavendeck/UndoManagerTest.kt
@@ -1,6 +1,7 @@
 package com.example.gloomhavendeck
 
 
+import com.example.gloomhavendeck.meta.Crap.Companion.allControllables
 import com.example.gloomhavendeck.meta.Logger
 import com.example.gloomhavendeck.meta.Saver
 import com.example.gloomhavendeck.meta.UndoManager
@@ -15,18 +16,23 @@ internal class UndoManagerTest {
     lateinit var controller: Controller
     lateinit var undoManager: UndoManager
 
+//    fun allExpectedToBeSaved(): Array<*> {
+//        // I don't know how to not do this by hand. aaaaaaaa
+//        val otherStuff = allControllables() as Array<*>
+//        return arrayOf(
+//            Card(),
+//            Enemy("dog 16 16"),
+//            Status.POISON,
+//            Item.LUCKY_EYE
+//        ) + otherStuff
+//    }
+
     @Test
     fun encodeEach() {
-        Json.encodeToString(Card())
-        Json.encodeToString(Deck())
-        Json.encodeToString(Enemy("dog 16 16"))
-        Json.encodeToString(Inventory())
-        Json.encodeToString(Logger())
-        Json.encodeToString(Player(maxHp = 26))
-        Json.encodeToString(Saver(filesDir = ""))
-        Json.encodeToString(UndoManager())
-        Json.encodeToString(UndoPoint())
-        Json.encodeToString(UndoPoint())
+        for (o in allControllables()) {
+            Json.encodeToString(o)
+        }
+        Json.encodeToString(Controller.newFullyStocked())
     }
 
     @Before
